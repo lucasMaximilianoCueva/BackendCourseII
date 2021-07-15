5e4bd3c557a04e0c74853d6f578591a32cae26cd
Lucas Cueva <lucasmaximiliano@outlook.com>
2021-07-15 16:15:04 -0300

    solucionado 29

---
+++ react-backend-ecommerce/Config/calc.js
@@ -0,0 +1,26 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let arr = [];
+
+const calculate = (n) => {
+  //cicle
+  let i = 0;
+  while (i < n) {
+    arr.push(getRandomInt(1, 1000));
+    i = i + 1;
+  }
+
+  let repeated = {};
+
+  arr.forEach(function (num) {
+    repeated[num] = (repeated[num] || 0) + 1;
+  });
+
+  return repeated;
+};
+
+process.on("message", (msg) => {
+  process.send(calculate(msg))
+});
--- react-backend-ecommerce/Config/passportConfig.js
+++ react-backend-ecommerce/Config/passportConfig.js
@@ -5,16 +5,16 @@ const facebookStrategy = facebook.Strategy;
 
 const passportConfig = (passport) => {
   passport.use(
-    new facebookStrategy({
-      clientID: '285367766706574',
-      clientSecret: '48cc6580360da394ca2fb3eee0ebe982',
+    new facebookStrategy({    // Obj Process
+      clientID: process.argv[3] || '285367766706574', 
+      clientSecret: process.argv[4] || '48cc6580360da394ca2fb3eee0ebe982',
       callbackURL: '/auth/facebook/callback',
       profileFields: ['id', 'displayName', 'photos', 'emails'],
       scope: ['email']
   }, function (accessToken, refreshToken, userProfile, done) {
       return done(null, userProfile);
   }));
-
+  
   passport.serializeUser(function (user, cb) {
     cb(null, user);
   });
